f0acf779f737e5b8e0517958c7cca340ae26125a
AmirHosseinFallahmanesh <54832303+AmirHosseinFallahmanesh@users.noreply.github.com>
2021-06-08 20:37:34 +0430

    Add project files.

---
+++ Demo1/Controllers/AccountController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo1.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(/*string username,string password*/   LoginViewModel model)
+        {
+
+            //string username = Request.Query["Username"];
+            //string password = Request.Query["Password"];
+            if (model.Username == "a@a" && model.Password=="123")
+            {
+                return Content("Login Successfully ");
+            }
+               return Content("Username or password not correct ");
+        }
+    }
+    public class LoginViewModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Demo1/Controllers/CustomController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo1.Controllers
+{
+    public class CustomController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string Index2() => "this is index2";
+        public void Index3()
+        { 
+        
+        
+        }
+    }
+}
